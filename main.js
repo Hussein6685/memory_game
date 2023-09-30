9c23fc27945e4d0858dad5a692c245b41319bbe2
Hussein6685 <113841959+Hussein6685@users.noreply.github.com>
2023-09-30 11:19:35 +0300

    css & js

---
+++ main.js
@@ -0,0 +1,11 @@
+document.querySelector(".control-buttons span").onclick = function () {
+    let yourName = prompt("Whats Your Name?");
+    // console.log(yourName);
+    if (yourName == null || yourName == "") {
+        document.querySelector(".name span").innerHTML = "Unknown";
+    } else {
+        document.querySelector(".name span").innerHTML = yourName;
+
+    }
+    document.querySelector(".control-buttons").remove();
+};
