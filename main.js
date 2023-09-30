4a89e0b08d8b8e1210e6bd123677c0b83e286eac
Hussein6685 <113841959+Hussein6685@users.noreply.github.com>
2023-09-30 11:55:39 +0300

    mian--js

--- main.js
+++ main.js
@@ -1,11 +1,28 @@
+// select the start game button
 document.querySelector(".control-buttons span").onclick = function () {
+//    prompt window to ask for name
     let yourName = prompt("Whats Your Name?");
     // console.log(yourName);
+    // if name is empty
     if (yourName == null || yourName == "") {
+    //     set name to unknown
         document.querySelector(".name span").innerHTML = "Unknown";
+    // name is not empty
     } else {
+        //  set name to your name
         document.querySelector(".name span").innerHTML = yourName;
 
     }
+    // remove splash screen
     document.querySelector(".control-buttons").remove();
 };
+
+let duration = 1000;
+
+let blockContainer = document.querySelector(".memory-game-blocks");
+
+let blocks = Array.from(blockContainer.children);
+
+let orderRange = [...Array(blocks.length).keys()];
+
+console.log(orderRange);
